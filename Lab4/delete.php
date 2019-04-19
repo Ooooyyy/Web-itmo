7ddeb9425423580ae1469a00cb5261af8f31f3bf
Ooooyyy <vladimirmanko@mail.ru>
2019-04-19 17:40:36 +0300

    Update delete.php

--- Lab4/delete.php
+++ Lab4/delete.php
@@ -13,4 +13,13 @@ if (isset($_GET['id'])){
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
+
+    $id_del = $_GET['id'];
+
+    $sql = "DELETE FROM news WHERE id = '$id_del'";
+    $result = mysqli_query($conn, $sql);
+
+    mysqli_close($conn);
+
+    header("Location: admin.php");
 }
