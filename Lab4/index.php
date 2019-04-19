1b607c6f4c4ad2640a9ef363109a02b6ed42ce2d
Ooooyyy <vladimirmanko@mail.ru>
2019-04-19 13:46:54 +0300

    Create index.php

---
+++ Lab4/index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width" />
+    <title>lab4</title>
+</head>
+<body>
+
+<?php
+
+$servername = "localhost";
+$username = "user";
+$password = "1234";
+$dbname = "lab4";
+$port = 8888;
+
+$conn = mysqli_connect($servername, $username, $password, $dbname, $port);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, news_title, news_text FROM news";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        echo '<h1>' . $row["news_title"] . '</h1>';
+        echo '<p>' . $row["news_text"] .  '</p>';
+    }
+} else {
+    echo "<h1>No news</h1>";
+}
+
+mysqli_close($conn);
+
+?>
+
+</body>
+</html>
